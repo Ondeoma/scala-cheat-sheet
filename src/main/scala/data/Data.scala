f12b7947e2c1a42ada683411cba404225d1c761f
Shogo Kagenaga <kagenaga@ondeoma.com>
2026-03-15 16:33:58 +0900

    :pencil: List追加

--- src/main/scala/data/Data.scala
+++ src/main/scala/data/Data.scala
@@ -27,10 +27,14 @@ object Data {
     List(
       F("List", "apply", Nil, List(T("n", "Int")), "A", listImg("apply.svg"), List(dangerT("例外"), infoT("1つ取得")), "n番目の要素の取得.<br>存在しない場合は例外."),
       F("List", "lift", Nil, List(T("n", "Int")), "Option[A]", listImg("lift.svg"), List(infoT("1つ取得")), "n番目の要素の取得.<br>存在しない場合はNone."),
+      
       F("List", "head", Nil, Nil, "A", listImg("head.svg"), List(dangerT("例外"), infoT("1つ取得")), "先頭要素の取得.<br>Nilの場合は例外."),
       F("List", "headOption", Nil, Nil, "Option[A]", listImg("headOption.svg"), List(infoT("1つ取得")), "先頭要素の取得.<br>Nilの場合はNone."),
       F("List", "last", Nil, Nil, "A", listImg("last.svg"), List(dangerT("例外"), infoT("1つ取得")), "末尾要素の取得.<br>Nilの場合は例外."),
       F("List", "lastOption", Nil, Nil, "Option[A]", listImg("lastOption.svg"), List(infoT("1つ取得")), "末尾要素の取得.<br>Niの場合はNone."),
+      
+      F("List", "tail", Nil, Nil, "List[A]", listImg("tail.svg"), List(dangerT("例外"), infoT("部分取得")), "先頭以外を取得.<br>Nilの場合は例外."),
+      F("List", "tails", Nil, Nil, "Iterator[List[A]]", listImg("tails.svg"), List(infoT("部分取得")), "元のListからNilになるまで先頭要素を0~n個削る過程のList群の取得."),
 
       F("List", "take", Nil, List(T("n", "Int")), "List[A]", listImg("take.svg"), List(infoT("部分取得")), "先頭からn件取得.<br>不足時は存在するところまで取得."),
       F("List", "takeRight", Nil, List(T("n", "Int")), "List[A]", listImg("takeRight.svg"), List(infoT("部分取得")), "末尾からn件取得.<br>不足時は存在するところまで取得.<br>新Listの要素は逆順に'ならない'."),
@@ -42,6 +46,13 @@ object Data {
       F("List", "slice", Nil, List(T("from", "Int"), T("until", "Int")), "List[A]", listImg("slice.svg"), List(infoT("部分取得")), "from ~ (until - 1)番目を取得.<br>範囲外は無視.<br>from >= untilの場合はNil."),
       F("List", "splitAt", Nil, List(T("n", "Int")), "(List[A], List[A])", listImg("splitAt.svg"), List(infoT("分割")), "n番目以降で分割.<br>不足時は存在するところまでとNilで分割."),
       F("List", "span", Nil, List(T("p", "A => Boolean")), "(List[A], List[A])", listImg("span.svg"), List(infoT("分割")), "pの結果がfalseになる要素以降で分割.<br>全てtrueの場合は全てとNilで分割"),
+
+      F("List", "filter", Nil, List(T("p", "A => Boolean")), "List[A]", listImg("filter.svg"), List(infoT("絞り込み")), "pがtrueになる要素を取得."),
+      F("List", "filterNot", Nil, List(T("p", "A => Boolean")), "List[A]", listImg("filterNot.svg"), List(infoT("絞り込み")), "pがfalseになる要素を取得."),
+
+      F("List", "distinct", Nil, Nil, "List[A]", listImg("distinct.svg"), List(infoT("絞り込み"), infoT("重複排除")), "重複を排除したListを取得.<br>重複分は先頭に近い方を維持."),
+      F("List", "distinctBy", List("B"), List(T("f", "A => B")), "List[A]", listImg("distinctBy.svg"), List(infoT("絞り込み"), infoT("重複排除")), "fで変換した結果の重複を排除したListを取得.<br>重複分は先頭に近い方を維持."),
+      
       
       F("List", "map", List("B"), List(T("f", "A => B")), "List[B]", listImg("map.svg"), List(infoT("高階維持"), infoT("変換")), "全ての値をfで変換."),
     )
